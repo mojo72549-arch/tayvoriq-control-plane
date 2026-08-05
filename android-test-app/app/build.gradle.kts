acc515df51c428ed9487d73eaf31d3099bbacdb5
mojo72549-arch <mojo72549@gmail.com>
2026-08-05 22:27:49 +0200

    android test: configure Android app

---
+++ android-test-app/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.tayvoriq.addonmanager"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.tayvoriq.addonmanager.test"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 3
+        versionName = "1.2-test"
+        manifestPlaceholders["admobAppId"] = "ca-app-pub-3940256099942544~3347511713"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-ads:25.4.0")
+}
